caaf5365015cf88dd2bb615209e27b6e4ba6416f
nnphong1904 <45620830+nnphong1904@users.noreply.github.com>
2019-06-15 03:56:27 +0700

    Portrait Fixing

--- app/src/main/java/com/example/book_lib/BookAdapter.java
+++ app/src/main/java/com/example/book_lib/BookAdapter.java
@@ -1,17 +1,26 @@
 package com.example.book_lib;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
+
     private ArrayList<Book> books;
+    private RecyclerView mRecylerview;
+    private Context myContext;
+    private int landscape=2131165268;
+    private int portrait=2131165269;
     ItemClicked activity;
     public interface ItemClicked{
         void onItemClicked(int index);
@@ -21,6 +30,7 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
     public BookAdapter(Context context, ArrayList<Book> list){
 
         this.books=list;
+        this.myContext=context;
         activity=(ItemClicked) context;
 
     }
@@ -34,7 +44,21 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    activity.onItemClicked(books.indexOf((Book) itemView.getTag()));
+                    if (mRecylerview.getId()==landscape)
+                    {
+                        System.out.println("Landscape mode");
+                        activity.onItemClicked(books.indexOf((Book) itemView.getTag()));
+
+                    }
+                    else {
+                        System.out.println("Portrait mode");
+                        activity.onItemClicked(books.indexOf((Book) itemView.getTag()));
+                        Intent intent=new Intent(myContext,Second_activity_portrait.class);
+                        myContext.startActivity(intent);
+
+                    }
+
+
                 }
             });
         }
@@ -44,6 +68,7 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
     @Override
     public BookAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View v= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.rowlayout,viewGroup,false);
+
         return new ViewHolder(v);
     }
 
@@ -51,6 +76,14 @@ public class BookAdapter extends RecyclerView.Adapter<BookAdapter.ViewHolder> {
     public void onBindViewHolder(@NonNull BookAdapter.ViewHolder viewHolder, int i) {
         viewHolder.itemView.setTag(books.get(i));
         viewHolder.bookName.setText(books.get(i).getName());
+
+
+    }
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+
+        mRecylerview = recyclerView;
     }
 
     @Override
--- app/src/main/java/com/example/book_lib/Second_activity_portrait.java
+++ app/src/main/java/com/example/book_lib/Second_activity_portrait.java
@@ -1,36 +1,27 @@
 package com.example.book_lib;
 
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements BookAdapter.ItemClicked {
+public class Second_activity_portrait extends AppCompatActivity implements BookAdapter.ItemClicked {
     TextView nameFrag,authorFrag,summaryFrag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        if (findViewById(R.id.landscape_layout)!=null){
-            nameFrag=findViewById(R.id.nameBookFrag);
-            authorFrag=findViewById(R.id.authorFrag);
-            summaryFrag=findViewById(R.id.summaryFrag);
+        setContentView(R.layout.activity_second_portrait);
+        if(findViewById(R.id.portrait_layout)!=null) {
+            nameFrag = findViewById(R.id.nameBookFrag);
+            authorFrag = findViewById(R.id.authorFrag);
+            summaryFrag = findViewById(R.id.summaryFrag);
             onItemClicked(0);
         }
-
     }
-
-
-
 
     @Override
     public void onItemClicked(int index) {
         nameFrag.setText(ApplicationClass.bookArrayList.get(index).getName());
         authorFrag.setText(ApplicationClass.bookArrayList.get(index).getAuthor());
         summaryFrag.setText(ApplicationClass.bookArrayList.get(index).getSummary());
-
     }
 }
